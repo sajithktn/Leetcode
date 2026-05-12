71cc6709eae2aa819e4df2121db2bd15ae908027
sajithktn <sajithsak3610@gmail.com>
2026-05-12 10:08:25 +0530

    Add solution for Single Number

---
+++ LeetCode/Single-Number/Solution.js
@@ -0,0 +1,13 @@
+1/**
+2 * @param {number[]} nums
+3 * @return {number}
+4 */
+5var singleNumber = function(nums) {
+6    let result = 0;
+7
+8    for (let num of nums) {
+9        result = result ^ num;
+10    }
+11
+12    return result;
+13};
